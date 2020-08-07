44acaf4c6159ca6b22e448b6eeb990dde5079bc6
Sheikh Ali Raza <aliraza147.ar@gmail.com>
2020-08-08 03:23:55 +0500

    Correct
    new update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App({name}) {
 return <div>
           Hello World My Name is <strong> {name} </strong>
+          <h1>This is heading one from app.js</h1>
+          <h2>This is h2 from app.js</h2>
+          <br/>
+            <ul>
+              <li>UL List 1</li>
+              <li>UL List 2</li>
+              <li>Ul List 3</li>
+            </ul>
           
+            <ol>
+              <li>OL List 1</li>
+              <li>OL List 2</li>
+              <li>Ol List 3</li>
+            </ol>
         </div>
 }
 
